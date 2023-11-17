01b2e161b0085105b755f4c9033c62cb9e49f44e
alexPisar <support.db@wera-rus.com>
2023-11-17 17:11:24 +1000

    Сделал выбор сертификата с подписанием

--- AuthoritySignClient/AuthoritySignClient/CertChangeWindow.xaml.cs
+++ AuthoritySignClient/AuthoritySignClient/CertChangeWindow.xaml.cs
@@ -22,12 +22,27 @@ namespace AuthoritySignClient
     public partial class CertChangeWindow : Window
     {
         private IEnumerable<string> _inns;
+        private Utils.CertUtil _certUtil;
 
         public CertChangeWindow(IEnumerable<string> inns)
         {
             InitializeComponent();
-            DataContext = new ListViewModel<X509Certificate2>();
             _inns = inns;
+
+            var client = new System.Net.WebClient();
+
+            if (Utils.ConfigSet.Config.GetInstance().ProxyEnabled)
+            {
+                var webProxy = new System.Net.WebProxy();
+
+                webProxy.Address = new Uri("http://" + Utils.ConfigSet.Config.GetInstance().ProxyAddress);
+                webProxy.Credentials = new System.Net.NetworkCredential(Utils.ConfigSet.Config.GetInstance().ProxyUserName,
+                    Utils.ConfigSet.Config.GetInstance().ProxyUserPassword);
+
+                client.Proxy = webProxy;
+            }
+            _certUtil = new Utils.CertUtil(client);
+            DataContext = new ListViewModel<X509Certificate2>();
         }
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
@@ -50,6 +65,10 @@ namespace AuthoritySignClient
             {
                 if(openFileDialog.ShowDialog() == true)
                 {
+                    var contentBytes = System.IO.File.ReadAllBytes(openFileDialog.FileName);
+                    var signature = _certUtil.Sign((DataContext as ListViewModel<X509Certificate2>).SelectedItem, contentBytes, true);
+                    System.IO.File.WriteAllBytes($"{openFileDialog.FileName}.sig", signature);
+
                     var loadWindow = new LoadWindow();
                     loadWindow.Owner = this;
                     loadWindow.SetSuccessfulLoad("Файл SIG успешно сохранён.");
@@ -65,9 +84,15 @@ namespace AuthoritySignClient
 
         private void Window_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
+            var personalCertificates = _certUtil.GetAllGostPersonalCertificates();
+            var certs = personalCertificates.Where(c => _inns.Any(i => i == _certUtil.GetOrgInnFromCertificate(c)) && _certUtil.IsCertificateValid(c) && c.NotAfter > DateTime.Now).OrderByDescending(c => c.NotBefore);
+
+            (DataContext as ListViewModel<X509Certificate2>).ItemsList = new System.Collections.ObjectModel.ObservableCollection<X509Certificate2>(certs);
             (DataContext as ListViewModel<X509Certificate2>).SelectedItem = null;
+
             (DataContext as ListViewModel<X509Certificate2>).OnPropertyChanged("SelectedItem");
             (DataContext as ListViewModel<X509Certificate2>).OnPropertyChanged("ItemsList");
         }
+
     }
 }
---
+++ AuthoritySignClient/AuthoritySignClient/Utils/CertUtil.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Cryptography.WinApi;
+using Cryptography;
+
+namespace AuthoritySignClient.Utils
+{
+    public class CertUtil
+    {
+        private WinApiCryptWrapper _crypto = new WinApiCryptWrapper();
+        private System.Net.WebClient _client = null;
+
+        public CertUtil() { }
+
+        public CertUtil(System.Net.WebClient client)
+        {
+            _client = client;
+        }
+
+        public string GetOrgInnFromCertificate(X509Certificate2 certificate)
+        {
+            var inn = ParseCertAttribute(certificate.Subject, "ИНН").TrimStart('0');
+
+            if (string.IsNullOrEmpty(inn) || inn.Length == 12)
+            {
+                _crypto.InitializeCertificate(certificate);
+                inn = _crypto.GetValueBySubjectOid("1.2.643.100.4");
+            }
+
+            return inn;
+        }
+
+        public string ParseCertAttribute(string certData, string attributeName)
+        {
+            string result = String.Empty;
+            try
+            {
+                if (certData == null || certData == "") return result;
+
+                attributeName = attributeName + "=";
+
+                if (!certData.Contains(attributeName)) return result;
+
+                int start = certData.IndexOf(attributeName);
+
+                if (start > 0 && !certData.Substring(0, start).EndsWith(" "))
+                {
+                    attributeName = " " + attributeName;
+
+                    if (!certData.Contains(attributeName)) return result;
+                }
+
+                start = certData.IndexOf(attributeName) + attributeName.Length;
+
+                int length = certData.IndexOf('=', start) == -1 ? certData.Length - start : certData.IndexOf(", ", start) - start;
+
+                if (length == 0) return result;
+                if (length > 0)
+                {
+                    result = certData.Substring(start, length);
+
+                }
+                else
+                {
+                    result = certData.Substring(start);
+                }
+                return result;
+
+            }
+            catch (Exception)
+            {
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Проверка сертификата на валидность, и что он не содержится в списках отзыва
+        /// </summary>
+        /// <param name="certificate"></param>
+        /// <returns></returns>
+        public bool IsCertificateValid(X509Certificate2 certificate, Logger.UtilityLog log = null)
+        {
+            log?.Log($"IsCertificateValid : проверка на валидность сертификата с серийным номером {certificate.SerialNumber}");
+            _crypto.InitializeCertificate(certificate);
+
+            var isCertValid = _crypto.IsCertificateValid(_client);
+            log?.Log($"Результат проверки на наличие в списках отзыва - {(!isCertValid).ToString()}");
+            return isCertValid;
+        }
+
+        public List<X509Certificate2> GetAllGostPersonalCertificates()
+        {
+            return _crypto.GetAllGostPersonalCertificates();
+        }
+
+        public byte[] Sign(X509Certificate2 signerCertificate, byte[] fileBytes, bool isDetached)
+        {
+            _crypto.InitializeCertificate(signerCertificate);
+            var signature = _crypto.Sign(fileBytes, isDetached);
+            return signature;
+        }
+    }
+}
